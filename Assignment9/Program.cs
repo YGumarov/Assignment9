330711ad8330fc8bb0a9c7d75b95e7f5643ba801
YGumarov <yerassylgumarov@gmail.com>
2023-06-03 09:00:16 +0600

    plz plz plz

---
+++ Assignment9/Program.cs
@@ -0,0 +1,30 @@
+﻿public class Edge<T>
+{
+    public T Source { get; }
+    public T Destination { get; }
+    public double Weight { get; }
+
+    public Edge(T source, T destination, double weight)
+    {
+        Source = source;
+        Destination = destination;
+        Weight = weight;
+    }
+}
+
+public class Vertex<T>
+{
+    public T Data { get; }
+    public Dictionary<Vertex<T>, double> AdjacentVertices { get; }
+
+    public Vertex(T data)
+    {
+        Data = data;
+        AdjacentVertices = new Dictionary<Vertex<T>, double>();
+    }
+
+    public void AddAdjacentVertex(Vertex<T> destination, double weight)
+    {
+        AdjacentVertices[destination] = weight;
+    }
+}
